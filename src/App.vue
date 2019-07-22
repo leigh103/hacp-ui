f65ee6fccb461d7a46552d3afbd14ff1c6f4cc71
Lee Anderson <lee@reformedreality.com>
2019-07-22 22:56:54 +0100

    Adjusting viewport animation

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,14 @@
     <div id="app" class="vh100">
 
         <div class="view-menu main-menu" :class="{'collapse':view.selected_room || view.selected_device || view.selected_group || view.selected_alarm !== false || view.selected_sensor}">
-            <router-link to="/" :class="{'selected': !$route.name}"><i class="fas fa-home"></i></router-link>
-            <router-link to="/rooms" :class="{'selected': $route.name == 'rooms'}"><i class="fas fa-vector-square"></i></router-link>
-            <router-link to="/groups" :class="{'selected': $route.name == 'groups'}"><i class="fas fa-layer-group"></i></router-link>
-            <router-link to="/lights" :class="{'selected': $route.name == 'lights'}"><i class="fas fa-lightbulb"></i></router-link>
-            <router-link to="/sensors" :class="{'selected': $route.name == 'sensors'}"><i class="fas fa-eye"></i></router-link>
-            <router-link to="/switches" :class="{'selected': $route.name == 'switches'}"><i class="fas fa-toggle-on"></i></router-link>
-            <router-link to="/devices" :class="{'selected': $route.name == 'devices'}"><i class="fas fa-mobile"></i></router-link>
-            <router-link to="/alarms" :class="{'selected': $route.name == 'alarms'}"><i class="fas fa-bell"></i></router-link>
+            <router-link to="/" :class="{'selected': !$route.name}"><i class="fas fa-home mr-1"></i>Home</router-link>
+            <router-link to="/rooms" :class="{'selected': $route.name == 'rooms'}"><i class="fas fa-vector-square mr-1"></i>Rooms</router-link>
+            <router-link to="/groups" :class="{'selected': $route.name == 'groups'}"><i class="fas fa-layer-group mr-1"></i>Groups</router-link>
+            <router-link to="/lights" :class="{'selected': $route.name == 'lights'}"><i class="fas fa-lightbulb mr-1"></i>Lights</router-link>
+            <router-link to="/sensors" :class="{'selected': $route.name == 'sensors'}"><i class="fas fa-eye mr-1"></i>Sensors</router-link>
+            <router-link to="/switches" :class="{'selected': $route.name == 'switches'}"><i class="fas fa-toggle-on mr-1"></i>Switches</router-link>
+            <router-link to="/devices" :class="{'selected': $route.name == 'devices'}"><i class="fas fa-mobile mr-1"></i>Devices</router-link>
+            <router-link to="/alarms" :class="{'selected': $route.name == 'alarms'}"><i class="fas fa-bell mr-1"></i>Alarms</router-link>
         </div>
 
         <div class="view-viewport" :class="{'expand':view.selected_room || view.selected_device || view.selected_group || view.selected_alarm !== false  || view.selected_sensor}">
