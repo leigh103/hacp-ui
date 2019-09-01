654c77fa73204447ec44069885fd0a7afc8920a8
Lee Anderson <lee@reformedreality.com>
2019-09-01 11:53:16 +0100

    Adding ability to pass group object, or group ID

--- src/components/GroupToggle.vue
+++ src/components/GroupToggle.vue
@@ -1,30 +1,56 @@
 <template>
-    <div class="group-toggle" @click.prevent="toggle()" v-if="group" :class="{'on':group.state.any_on === true,'off':group.state.any_on === false,'unavailable':group.state.reachable === false}">
-        <div class="indicator"></div>
-        <p>{{ group.name }}</p>
+
+
+    <div class="toggle" @click.prevent="toggle()" v-if="group" :class="{'on':group.state.any_on === true,'off':group.state.any_on === false,'unavailable':group.state.reachable === false}">
+        <div class="indicator">
+            <div class="led"></div>
+        </div>
+        <div class="name text-bold">{{ group.name }}</div>
     </div>
+
+    <div class="toggle" @click.prevent="toggle()" v-else-if="id" :class="{'on':groups[id].state.any_on === true,'off':groups[id].state.any_on === false,'unavailable':groups[id].state.reachable === false}">
+        <div class="indicator">
+            <div class="led"></div>
+        </div>
+        <div class="name text-bold">{{ groups[id].name }}</div>
+    </div>
+
+
 </template>
 
 <script>
 
-    module.exports = {
+    import { mapState } from 'vuex'
+
+    export default {
         props: ['group', 'id'],
         data() {
             return {
                 //nothing
             }
         },
+        computed: mapState([
+                'groups'
+            ]),
         methods: {
             toggle() {
 
+                if (!this.group){ // if the group prop isn't passed
+                    var any_on = this.groups[this.id].state.any_on
+                    var gid = this.id
+                } else {
+                    var any_on = this.group.state.any_on
+                    var gid = this.group.id
+                }
+
                 var action = true
 
-                if (this.group.state.any_on === true){
+                if (any_on === true){
                     action = false
                 }
 
                 let payload = {
-                    url:'groups/'+this.key+'/action',
+                    url:'groups/'+gid+'/action',
                     data:{
                         on:action
                     }
@@ -33,6 +59,13 @@
                 this.$store.dispatch('call',payload)
 
             }
+        },
+        mounted(){
+
+            if (Object.keys(this.groups).length == 0 && Object.keys(this.group).length == 0){
+                this.$store.dispatch('getEntities','groups')
+            }
+
         }
     }
 
