ccb367010fcd887c8345d492d9b9a16e3f832fd5
Lee Anderson <lee@reformedreality.com>
2019-09-01 11:53:57 +0100

    Home page layout tweaks

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,14 @@
 <template>
-  <div class="about p-2 text-left">
-    <h1 v-if="weather.currently.apparentTemperature">Good {{time.time_of_day}}, it's {{weather.currently.apparentTemperature | temp}}&deg;</h1>
+  <div class="about p-2 text-left overflow-y">
+    <h1 v-if="weather">Good {{time.time_of_day}}, it's {{weather.currently.apparentTemperature | temp}}&deg;</h1>
     <h2>{{weather.daily.summary}}</h2>
+
+    <div v-if="fav_groups.length > 0">
+        <h2 class="mt-2 mb-1">Groups</h2>
+        <div class="scroll-horizontal">
+            <group-toggle v-for="(val,gid) in fav_groups" :id="val.gid"></group-toggle>
+        </div>
+    </div>
   </div>
 </template>
 
@@ -12,9 +19,16 @@ import { mapState } from 'vuex'
 
 export default {
     name: 'home',
+    data () {
+        return {
+            favorites: {},
+            fav_groups: []
+        }
+    },
     computed: mapState([
         'weather',
-        'time'
+        'time',
+        'groups'
     ]),
     filters: {
         temp (value) {
@@ -23,6 +37,22 @@ export default {
     },
     mounted () {
         this.$store.dispatch('all')
+
+        this.favorites = localStorage.getItem('favorites')
+
+        if (this.favorites.length > 0){
+            this.favorites = JSON.parse(this.favorites)
+
+            if (this.favorites.selected_group){
+                for (let i in this.favorites.selected_group){
+                    this.fav_groups.push({gid:i,count:this.favorites.selected_group[i]})
+                    this.fav_groups.sort(function(a, b){
+                      return a.count < b.count;
+                    })
+                }
+            }
+        }
+
     },
 }
 </script>
